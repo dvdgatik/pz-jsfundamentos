abf624b4a53629eb7caf30ba9725bfd4e445ac58
David Gatica <dvd_gatik@hotmail.com>
2023-07-08 13:39:43 -0600

    Objetos

---
+++ objects.js
@@ -0,0 +1,25 @@
+/*Objetos: JS es un lenguaje que está diseñado en un paradigma de objetos.
+
+Ejemplo de Objeto:*/
+
+
+var miAuto = {
+    marca: "Toyota",
+    modelo: "Corolla",
+    año: 2020
+}
+
+
+//Acceder a la propiedad de un objeto
+
+
+miAuto.marca
+miAuto['marca'];
+    
+
+
+
+
+// los key values se representan como string para JS esto quiere decir que si colocan una propiedad por ejemplo marca : “toyota” es lo mismo decir “marca” : “toyota”
+
+// Debido a que los key values JS los interpreta como strings significa que tambien pueden acceder a una propiedad usando llaves y dentro especificando el key value
