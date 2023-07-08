e688ad07cb8e1c5e79b2093db681f0b62b4112c2
David Gatica <dvd_gatik@hotmail.com>
2023-07-08 02:05:43 -0600

    Switch Sentence

---
+++ switch.js
@@ -0,0 +1,25 @@
+var pregunta = prompt("Ingresa tu opción: piedra, papel o tijera "); 
+var user = pregunta.toLowerCase();
+var options = ["piedra", "papel", "tijera"];
+var machine = options[Math.floor(Math.random() * 3)];
+
+
+// let numero = 'a';
+//con true los casos van a pasar 
+// Es importante declarar el break; de lo contrario todas los casos se ejecutaran
+switch (true) {
+    case (user === machine):
+        console.log('es un empate');
+        break;
+    case (machine === 'piedra' && user === 'papel'):
+        console.log('Ganaste')
+        break;
+    case (machine === 'papel'  && user === 'tijera'):
+        console.log('Ganaste')
+        break;
+    case (machine === 'tijera' && user === 'piedra'):
+        console.log('Ganaste')
+        break;
+    default:
+        console.log('¡Perdiste!');       
+}
