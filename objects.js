56c015e4d5c573e49b8bc488906ba4ceade633e9
David Gatica <dvd_gatik@hotmail.com>
2023-07-08 14:06:39 -0600

    Objetos

--- objects.js
+++ objects.js
@@ -17,6 +17,22 @@ miAuto.marca
 miAuto['marca'];
     
 
+var miLaptop = {
+    Modelo: "15-dw0004la",
+    Procesador: "Intel Core i7",
+    MemoriaRAM: "8GB",
+    DiscoDuro: "256GB",
+    Peso: "1.74 kg",
+    caracteristicas: function(){
+        console.log(`Mi laptop tiene las siguientes caracteristicas ${this.Modelo} ${this.Procesador} ${this.MemoriaRAM} ${this.DiscoDuro} ${this.Peso}`)
+    }
+}
+
+miLaptop.caracteristicas()
+
+
+//this es una palabra clave que hace referencia al objeto padre
+
 
 
 
